53c98848ef74d998c333bbfd4f9e1997978eb0fc
Marian Mutu <marian.mutu02@gmail.com>
2024-12-27 12:28:09 +0200

    revoved duplicates

--- internship-app-fe/src/app/core/models/notification.model 2.ts
+++
@@ -1,8 +0,0 @@
-export interface Notification {
-    notificationId: string;
-    userId: string;
-    internshipId: string;
-    message: string;
-    readStatus: boolean;
-    timestamp: Date;
-  }
