110d2325dfb5017c29bf0e4540ab79d26bcea5e0
Marian Mutu <marian.mutu02@gmail.com>
2024-12-29 15:57:56 +0200

    dashboard integration with internships

--- internship-fe-app/src/app/features/dashboard/dashboard-header/dashboard-header.component.ts
+++ internship-fe-app/src/app/features/dashboard/dashboard-header/dashboard-header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router'; // Import RouterModule
 
 @Component({
   selector: 'app-dashboard-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule], // Add RouterModule to imports
   templateUrl: './dashboard-header.component.html',
-  styleUrl: './dashboard-header.component.scss'
+  styleUrls: ['./dashboard-header.component.scss']
 })
 export class DashboardHeaderComponent {
-
+  constructor(private router: Router) {}
 }
